d865953a1d7220b282a91a74b9eea600937b9cc3
Manuella Alcantara <manuellar902@gmail.com>
2025-08-06 15:53:17 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -47,5 +47,9 @@ def explicacao():
 def lucros():
     return render_template("lucros.html", historico=historico)
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
